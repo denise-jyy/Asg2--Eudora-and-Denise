53037accc1bfe4de057a298f1f444cc579e223a2
denise-jyy <S10243946@connect.np.edu.sg>
2023-01-27 17:56:46 +0800

    post signup database (index)

--- script.js
+++ script.js
@@ -65,4 +65,38 @@ cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
 
+/* Signup and Login database*/
 
+$(document).ready(function(){
+    const APIKEY = "63d37ccd3bc6b255ed0c4356";
+    
+    //event listener
+    $("#submit-button").on("click", function(e){
+        e.preventDefault();
+
+        var username = $("#username").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        var jsondata = {"username": username,"email": email,"password": password};
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://learnwithblook-2399.restdb.io/rest/login",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondata)
+        }
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+      
+          });
+    })
+    
+})
