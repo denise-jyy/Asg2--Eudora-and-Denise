db6dd9b62666d2f5bb1624ffc20f914301ae53e1
denise-jyy <S10243946@connect.np.edu.sg>
2023-02-10 18:01:11 +0800

    HTML and CSS validation

--- game.js
+++ game.js
@@ -54,7 +54,7 @@ function shuffleCard() {
     cards.forEach((card, i) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
-        imgTag.src = `Images/Memory game/img-${arr[i]}.png`;
+        imgTag.src = `Images/Memorygame/img-${arr[i]}.png`;
         card.addEventListener("click", flipCard);
     });
 }
